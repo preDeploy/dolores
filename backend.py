4379d45b5ed031767dccab0e48bc0046f2d8076e
preDeploy <168213155+preDeploy@users.noreply.github.com>
2024-04-30 22:24:08 -0700

    Add files via upload

--- backend.py
+++ backend.py
@@ -11,35 +11,28 @@ class Dolores():
     def __init__(self):
         self.OPENAI_API_KEY = ""
         self.OPENAI_ASSISTANT_ID = ""
-        self.user_input = ''
-        self.response = ''
-        self.flag = True
-        self.user = ''
-        self.user_context = ''
-        self.assistant_context = ''
         self.context = {
             "user": '',
             "assistant": ''
         }
         self.location = ''
 
-    def assistant(self):        
+    def assistant(self, user_input):
         client = OpenAI(api_key=self.OPENAI_API_KEY)
         thread = client.beta.threads.create(
             messages=[
                 {
                     "role": "user",
-                    "content": f"This is the previous context:\n\nuser:{self.context['user']}\nassistant:{self.context['assistant']}. And this is the current prompt: {self.user_input}. Respond in plain string format, no special formatting",
+                    "content": f"This is the previous context:\n\nuser:{self.context['user']}\nassistant:{self.context['assistant']}. And this is the current prompt: {user_input}. Respond in plain string format, no special formatting",
                 }
             ]
         )
 
         run = client.beta.threads.runs.create(
-            thread_id = thread.id,
-            assistant_id = self.OPENAI_ASSISTANT_ID
+            thread_id=thread.id,
+            assistant_id=self.OPENAI_ASSISTANT_ID
         )
         while run.status != "completed":
-            print(run.status)
             run = client.beta.threads.runs.retrieve(
                 thread_id=thread.id,
                 run_id=run.id
@@ -48,18 +41,18 @@ class Dolores():
             thread_id=thread.id
         )
         return messages.data[0].content[0].text.value
-    
+
     def generateContext(self, input, role):
         summarizing_client = OpenAI(api_key=self.OPENAI_API_KEY)
         response = summarizing_client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {
-                    "role":"system",
-                    "content": f"You'll be given a transcript of one of the person in the chat. this transcript is from the {role}. The transcript will have 2 paragraphs, first paragraph will have all the summarized info from previous message sent by the {role} and current message sent by the {role}. You have to summarize both these messages and create a paragraph that will have all the important information from the transcripts. Return summary in this format: 'Previous Context:'"
+                    "role": "system",
+                    "content": f"You'll be given a transcript of one of the person in the chat. this transcript is from the {role}. The transcript will have 2 paragraphs, first paragraph will have all the summarized info from previous message sent by the {role} and current message sent by the {role}. You have to summarize both these messages and create a paragraph that will have all the important information from the transcripts. Return summary in this format: 'Previous Context: [summary]'"
                 },
                 {
-                    "role":"user",
+                    "role": "user",
                     "content": f"{input}"
                 }
             ],
@@ -67,32 +60,29 @@ class Dolores():
             top_p=1
         )
         return response.choices[0].message.content
-    
-    def openJSON(self, user):        
-        self.user = str(user)
+
+    def openJSON(self, user):
         if not os.path.isdir(f"./context/{user}"):
             os.makedirs(f"./context/{user}")
         self.location = f"./context/{user}/{datetime.today().strftime('%Y%m%d')}.json"
         try:
             with open(self.location, 'r') as cFile:
-                self.context=json.load(cFile)
+                self.context = json.load(cFile)
         except:
             pass
-    
+
     def saveJSON(self):
         with open(self.location, 'w') as cFile:
             json.dump(self.context, cFile)
-    
+
     def chat(self, user_input, user):
-        self.user_input = user_input
         self.openJSON(user)
-        self.response = self.assistant().replace("\n", "<br>")
-        user_context = self.generateContext(f"{self.context['user']}\n\n{self.user_input}", "user")
-        assistant_context = self.generateContext(f"{self.context['assistant']}\n\n{self.response}", "assistant")
-        self.context['user'] = user_context
-        self.context['assistant'] = assistant_context
+        response = self.assistant(user_input).replace("\n", "<br>")
+        self.context['user'] = self.generateContext(f"{self.context['user']}\n\n{user_input}", "user")
+        self.context['assistant'] = self.generateContext(f"{self.context['assistant']}\n\n{response}", "assistant")
         self.saveJSON()
-        return self.response
+        return response
+
 
 app = FastAPI()
 origins = [
@@ -108,7 +98,7 @@ app.add_middleware(
 )
 
 bot_responses = Dolores()
-# print(f'''Assistant: {bot_responses.chat(f"{input('You: ')}", "rajanpande")}''')
+
 
 @app.post("/get_response/")
 async def get_bot_response(request: Request):
@@ -116,8 +106,8 @@ async def get_bot_response(request: Request):
     user_message = data["user_message"]
     user = data["user"]
     bot_response = bot_responses.chat(user_message, user)
-    print(bot_response)
     return {"bot_response": bot_response}
 
+
 if __name__ == "__main__":
-    uvicorn.run("backend:app", reload=True)
+    uvicorn.run("backend:app", host='0.0.0.0', reload=True)
